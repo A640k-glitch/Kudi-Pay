1827d7677215c8661effeddbfd51248ec2918e96
A640k-glitch <akinabraham4@gmail.com>
2026-07-16 16:37:33 +0100

    feat: add Tailwind styles and implement root application rendering with ToastProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './minified.css';
 import { ToastProvider } from './components/Toast';
 
 createRoot(document.getElementById('root')!).render(
